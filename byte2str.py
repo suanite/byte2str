83515e2d7220b1e12b12e8d091a41204b106b935
lyq <15989087423@163.com>
2017-12-26 08:59:00 +0800

    实现汉字和十六进制字符串之间的相互转换，下一步的工作是文本框绑定鼠标右键，添加复制、剪切、粘贴功能

--- byte2str.py
+++ byte2str.py
@@ -7,6 +7,8 @@ import re
 
 
 class Byte2str(object):
+
+    # 初始化方法，创建界面
     def __init__(self, master=None):
         self.flag = BooleanVar()
 
@@ -26,9 +28,10 @@ class Byte2str(object):
         self.str_text.grid(row=1, column=2, rowspan=2, padx=5, pady=5, sticky='w')
         self.character_checkbutton.grid(row=4, column=0, sticky='w')
 
+    # 将普通的字符串转成字节流，再把字节流按指定编码转换成汉字
     def byte2str(self):
         byte_data = self.byte_text.get('1.0', 'end')
-        byte_data = re.sub('[\n\r\t ]', '', byte_data).upper()
+        byte_data = re.sub(r'[\n\r\t ]', '', byte_data).upper()
 
         base = '0123456789ABCDEF'  # 将字符转成对应的16进制数
         i = 0
@@ -51,10 +54,22 @@ class Byte2str(object):
         self.str_text.delete('1.0', 'end')
         self.str_text.insert('1.0', str_data)
 
-    # 字符串转字节流还没实现
+    # 把汉字按指定的编码传承十六进制的字符串
     def str2byte(self):
-        foo = self.str_text.get('1.0', 'end')
-        print(foo.decode('cp936'))
+        str_data = self.str_text.get('1.0', 'end')  # 从文本框获取的是unicode对象，并非str类型
+        str_data = re.sub(r'[\n\r\t ]', '', str_data)
+
+        if self.flag.get():
+            byte_data = repr(str_data.encode('utf-8'))
+            byte_data = re.sub(r'[\'\\x]', '', byte_data).upper()
+
+        else:
+            byte_data = repr(str_data.encode('gbk'))
+            byte_data = re.sub(r'[\'\\x]', '', byte_data).upper()
+            print byte_data
+
+        self.byte_text.delete('1.0', 'end')
+        self.byte_text.insert('1.0', byte_data)
 
 
 if __name__ == '__main__':
